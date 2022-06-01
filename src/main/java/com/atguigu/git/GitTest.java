f1b520039b277433ea3f56daa3235a7f059ea10f
zwj121382 <83821686+zwj121382@users.noreply.github.com>
2022-06-01 20:33:42 +0800

    pull

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git4");
         System.out.println("3");
+        System.out.println("5");
     }
 }
